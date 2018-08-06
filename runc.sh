eb42a3e0a0ebe241568a437a02c32a167fddb72c
n9Mtq4 <bill.wresnahan@gmail.com>
2018-08-06 16:35:17 -0400

    running scripts

---
+++ runc.sh
@@ -0,0 +1,1 @@
+python3 -u nanodelinkbot.py comments | ts '[%Y-%m-%d %H:%M:%S]' | tee -a comments.log
---
+++ runs.sh
@@ -0,0 +1,1 @@
+python3 -u nanodelinkbot.py submissions | ts '[%Y-%m-%d %H:%M:%S]' | tee -a submissions.log
