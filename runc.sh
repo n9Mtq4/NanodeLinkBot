bc6518f2521a9873c3b54e4f79af178f70290cc5
n9Mtq4 <bill.wresnahan@gmail.com>
2018-08-31 20:46:43 -0400

    auto restart

--- runc.sh
+++ runc.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+# if it stops, restart after 10 minutes
+while true; do
 python3 -u nanodelinkbot.py comments | ts '[%Y-%m-%d %H:%M:%S]' | tee -a comments.log
+sleep 600
+done
--- runs.sh
+++ runs.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+# if it stops, restart after 10 minutes
+while true; do
 python3 -u nanodelinkbot.py submissions | ts '[%Y-%m-%d %H:%M:%S]' | tee -a submissions.log
+sleep 600
+done
